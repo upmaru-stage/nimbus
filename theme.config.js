436935629ab3edde28fdbdc7afea2c9887610412
Zack Siri <me@zacksiri.dev>
2023-04-19 10:15:10 +0700

    Update about

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
+        <time>{YEAR}</time> © Zack Siri.
         <a href="/feed.xml">RSS</a>
       </small>
       <style jsx>{`
