a692292a350dd559f8447c24ea116879c62d28d7
Zack Siri <me@zacksiri.dev>
2023-04-20 15:42:05 +0700

    Update gen rss

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    title: 'Zack Siri',
+    site_url: 'https://zacksiri.dev',
+    feed_url: 'https://zacksiri.dev/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
